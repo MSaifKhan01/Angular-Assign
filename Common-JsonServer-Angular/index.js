36b9732692bd89c955329713553ba7b1eb06c4bf
MSaifKhan01 <msaifkhan5038@gmail.com>
2023-08-24 01:41:14 +0530

    Common JsonServer for Angular

---
+++ Common-JsonServer-Angular/index.js
@@ -0,0 +1,17 @@
+const jsonServer=require("json-server")
+
+
+var cors = require('cors')
+require('dotenv').config();
+
+const server=jsonServer.create();
+const router=jsonServer.router("db.json")
+const middleware=jsonServer.defaults();
+const port =process.env.PORT||3001
+
+server.use(cors())
+server.use(middleware);
+server.use(router)
+server.listen(port,()=>{
+    console.log("connect")
+})
