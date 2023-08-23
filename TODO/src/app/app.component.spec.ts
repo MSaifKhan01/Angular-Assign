f5b71168f67c2fb64051b52ef4dffa8ba4115fe0
MSaifKhan01 <msaifkhan5038@gmail.com>
2023-08-23 17:51:43 +0530

    Angular01

--- TODO/src/app/app.component.spec.ts
+++ TODO/src/app/app.component.spec.ts
@@ -6,8 +6,12 @@ describe('AppComponent', () => {
     declarations: [AppComponent]
   }));
 
+
+
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
+
+    
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
